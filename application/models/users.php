b04b06194e0f376abb1b659b6c017ca5004a20aa
chirashree98 <83055275+chirashree98@users.noreply.github.com>
2021-04-26 14:04:23 +0600

    Create users.php

--- application/models/users.php
+++ application/models/users.php
@@ -13,6 +13,81 @@ public function insert($data,$table) {
 		
 		
 }
+	public function getproduct($postData=null){
+	ini_set('display_errors', '0'); 
+	 error_reporting(E_ALL);
+     	$response = array();
+
+     ## Read value
+     $draw = $postData['draw'];
+     $start = $postData['start'];
+     $rowperpage = $postData['length']; 
+     $columnIndex = $postData['order'][0]['column']; 
+     $columnName = $postData['columns'][$columnIndex]['data']; 
+     $columnSortOrder = $postData['order'][0]['dir']; 
+     $searchValue = $postData['search']['value']; 
+
+     ## Search 
+     $searchQuery = "";
+     if($searchValue != ''){
+        $searchQuery = " (name like '%".$searchValue."%' or description like '%".$searchValue."%' or price like'%".$searchValue."%' ) ";
+	 }
+	 
+	 	
+	
+     ## Total number of records without filtering
+     $this->db->select('count(*) as allcount');
+	  $this->db->where('user_id',$_SESSION['id']);
+     $records = $this->db->get('product')->result();
+     $totalRecords = $records[0]->allcount;
+	 	
+     ## Total number of record with filtering
+	 $this->db->select('count(*) as allcount');  
+	 //$this->db->where('user_id',$user_id);
+	 $this->db->where('user_id',$_SESSION['id']);
+     if($searchQuery != '')
+		
+        $this->db->where($searchQuery);
+     $records = $this->db->get('product')->result();
+     $totalRecordwithFilter = $records[0]->allcount;
+
+     ## Fetch records
+     $this->db->select('*');
+     if($searchQuery != '')
+        $this->db->where($searchQuery);
+     $this->db->order_by($columnName, $columnSortOrder); //$this->db->where('user_id',$user_id);
+	 //$this->db->where('user_id',$user_id);
+	 $this->db->where('user_id',$_SESSION['id']);
+	$this->db->limit($rowperpage, $start);
+     $records = $this->db->get('product')->result();
+
+     $data = array();
+
+     foreach($records as $record ){
+
+        $data[] = array( 
+			"id"=>$record->id,
+           "name"=>$record->name,
+           "image"=>$record->image,
+           "description"=>$record->description,
+           "price"=>$record->price,
+			'user_id'=>$record->user_id
+           
+        ); 
+     }
+
+     ## Response
+     $response = array(
+        "draw" => intval($draw),
+        "iTotalRecords" => $totalRecords,
+        "iTotalDisplayRecords" => $totalRecordwithFilter,
+        "aaData" => $data
+     );
+
+     return $response; 
+   }
+
+
 
 
 public function phone_check($phone){
